db586fa3580caf552dafad25a16b4592cfb77f48
CRICAPOGA <camilochoposada2@gmail.com>
2025-03-27 09:08:00 -0500

    add - Testing user model

--- RutasAndinas/Authentication/views.py
+++ RutasAndinas/Authentication/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import render
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
