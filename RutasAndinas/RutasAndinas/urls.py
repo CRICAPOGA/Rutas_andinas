6e71e75b63f3314f3096363a6a5792183abf5a7a
MarianaG22 <142903466+MarianaG22@users.noreply.github.com>
2025-03-27 11:51:37 -0500

    add - Rutas para la app 'Plans' y soporte para archivos estáticos en desarrollo

--- RutasAndinas/RutasAndinas/urls.py
+++ RutasAndinas/RutasAndinas/urls.py
@@ -17,9 +17,16 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
     path('authentication/', include('Authentication.urls')),
+    path('plans/', include('Plans.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
