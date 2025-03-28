68a53923c93d2cce34f9ec00321d65a01d1d1914
MarianaG22 <142903466+MarianaG22@users.noreply.github.com>
2025-03-28 18:42:49 -0500

    add - Editar reseña para el usuario (Sin diseño)

--- RutasAndinas/Reviews/urls.py
+++ RutasAndinas/Reviews/urls.py
@@ -3,4 +3,5 @@ from . import views
 app_name = 'reviews'
 urlpatterns = [
     path('<int:plan_id>/create/', views.create_review, name='create_review'),
+    path('<int:plan_id>/edit/<int:review_id>/', views.edit_review, name='edit_review'),
 ]
--- RutasAndinas/Reviews/views.py
+++ RutasAndinas/Reviews/views.py
@@ -28,3 +28,31 @@ def create_review(request, plan_id):
             messages.success(request, "Reseña creada exitosamente.")
             return redirect("detailsPlan", plan_id=plan_id)
     return redirect("detailsPlan", plan_id=plan_id)
+
+# Editar una reseña
+def edit_review(request, plan_id, review_id):
+    review = get_object_or_404(Review, pk=review_id)
+
+    # Verificar que el usuario actual es el creador de la reseña
+    if review.user_id != request.user:
+        messages.error(request, "No tienes permiso para editar esta reseña.")
+        return redirect("detailsPlan", plan_id=plan_id)
+
+    if request.method == "POST":
+        content = request.POST.get("content")
+        rate = int(request.POST.get("rate", 0))
+
+        # Validación
+        if not content:
+            messages.error(request, "El comentario no puede estar vacío.")
+        elif not (0 <= rate <= 5):
+            messages.error(request, "La calificación debe estar entre 0 y 5.")
+        else:
+            # Actualizar la reseña
+            review.content = content
+            review.rate = rate
+            review.save()
+            messages.success(request, "Reseña actualizada exitosamente.")
+            return redirect("detailsPlan", plan_id=plan_id)
+
+    return redirect("detailsPlan", plan_id=plan_id)
