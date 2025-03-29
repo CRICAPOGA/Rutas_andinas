ee9891ce3b6c1da607a8555287fa4f7de4eb7610
CRICAPOGA <camilochoposada2@gmail.com>
2025-03-29 16:03:53 -0500

    add - CRUD Usuarios

--- RutasAndinas/Users/urls.py
+++ RutasAndinas/Users/urls.py
@@ -1,4 +1,9 @@
 from django.urls import path
 from . import views
 
-urlpatterns = []
+urlpatterns = [
+    path('usuarios/', views.lista_usuarios, name='lista_usuarios'),
+    path('usuarios/crear/', views.crear_usuario, name='crear_usuario'),
+    path('usuarios/editar/<int:usuario_id>/', views.editar_usuario, name='editar_usuario'),
+    path('usuarios/eliminar/<int:usuario_id>/', views.eliminar_usuario, name='eliminar_usuario'),
+]
