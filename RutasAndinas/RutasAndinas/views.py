b39b6fbf8a6eefd56b98fc8d55511a85f448b8ed
Liseth Gomez <ljohannagomez@ucundinamarca.edu.co>
2025-03-31 20:30:49 -0500

    Eliminar cache y solución de la tabla

--- RutasAndinas/RutasAndinas/views.py
+++ RutasAndinas/RutasAndinas/views.py
@@ -11,6 +11,7 @@ def is_admin(user):
     return user.is_authenticated and user.is_staff
 
 @login_required
+@user_passes_test(is_admin)
 def financial_view(request):
     sales = Sale.objects.all().order_by('-sale_date') #Organizar por fecha descendente
     print(f"sales count: {sales.count()}")
