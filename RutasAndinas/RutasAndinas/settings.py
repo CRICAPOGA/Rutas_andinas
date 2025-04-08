c9bb95f4411d117ebe913052f7630d5b00a1d4e4
mcamila16 <mcamilacarrillo@ucundinamarca.edu.co>
2025-04-07 20:27:18 -0500

    add - Implementacion de imagenes y ajustes de diseño

--- RutasAndinas/RutasAndinas/settings.py
+++ RutasAndinas/RutasAndinas/settings.py
@@ -135,6 +135,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
     BASE_DIR / 'RutasAndinas' /'templates',
@@ -145,7 +146,6 @@ STATICFILES_DIRS = [
     BASE_DIR / 'Users' / 'static',
 ]
 MEDIA_URL = '/media/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
