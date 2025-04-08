11cd1527ddac665e60b6c0190c93248c4a3225d4
CRICAPOGA <camilochoposada2@gmail.com>
2025-04-08 08:38:06 -0500

    fix - Testing de la app Authentication

--- RutasAndinas/Authentication/test/test_authentication_views.py
+++ RutasAndinas/Authentication/test/test_authentication_views.py
@@ -3,9 +3,10 @@ from django.urls import reverse
 from django.contrib.auth import get_user_model
 from django.test import Client
 from Users.models import Role
+import Users
 
 @pytest.mark.django_db
-def test_login_view(client):
+def test_login_view(client: Client):
     response = client.get(reverse('login'))
     assert response.status_code == 200
     assert "login.html" in [t.name for t in response.templates]
@@ -13,21 +14,29 @@ def test_login_view(client):
 @pytest.mark.django_db
 def test_login_auth_success(client):
     User = get_user_model()
-    user = User.objects.create_user(username="testuser", password="testpassword")
-    
+    role_empleado = Role.objects.create(role="Empleado")
+
+    user = User.objects.create_user(
+        username="testuser",
+        password="testpassword",
+        role_id=role_empleado
+    )
+
     response = client.post(reverse('login_auth'), {'username': 'testuser', 'password': 'testpassword'})
-    assert response.status_code == 200
-    assert "index.html" in [t.name for t in response.templates]
+
+    # Verifica que redirige a 'list' si el usuario es 'Empleado'
+    assert response.status_code == 302
+    assert response.url == reverse('list')
 
 @pytest.mark.django_db
-def test_login_auth_fail(client):
+def test_login_auth_fail(client: Client):
     response = client.post(reverse('login_auth'), {'username': 'wronguser', 'password': 'wrongpass'})
     assert response.status_code == 200
     assert "login.html" in [t.name for t in response.templates]
     assert "Credenciales incorrectas" in response.content.decode()
 
 @pytest.mark.django_db
-def test_logout_view(client):
+def test_logout_view(client: Client):
     User = get_user_model()
     user = User.objects.create_user(username="testuser", password="testpassword")
     client.login(username="testuser", password="testpassword")
@@ -37,13 +46,13 @@ def test_logout_view(client):
     assert "login.html" in [t.name for t in response.templates]
 
 @pytest.mark.django_db
-def test_register_view_get(client):
+def test_register_view_get(client: Client):
     response = client.get(reverse('register'))
     assert response.status_code == 200
     assert "register.html" in [t.name for t in response.templates]
 
 @pytest.mark.django_db
-def test_register_view_post(client):
+def test_register_view_post(client: Client):
     Role.objects.create(role="User")
     response = client.post(reverse('register'), {
         'name': 'Test',
