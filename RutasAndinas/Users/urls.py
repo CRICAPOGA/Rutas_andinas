0faffb4967644ebd73a5b01f52afb8c96eeaf38e
mcamila16 <mcamilacarrillo@ucundinamarca.edu.co>
2025-04-07 21:09:53 -0500

    fix - Reseñas diseño en crudPlan

--- RutasAndinas/Users/urls.py
+++ RutasAndinas/Users/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('usuarios/', views.lista_usuarios, name='lista_usuarios'),
+    path('usuarios/', views.lista_usuarios, name='usuarios'),
     path('usuarios/crear/', views.crear_usuario, name='crear_usuario'),
     path('usuarios/editar/<int:usuario_id>/', views.editar_usuario, name='editar_usuario'),
     path('usuarios/eliminar/<int:usuario_id>/', views.eliminar_usuario, name='eliminar_usuario'),
